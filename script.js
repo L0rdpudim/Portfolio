ce0c7709b2d62d0551261a42998564dd8cbeb739
Miguel Esperança <108767669+L0rdpudim@users.noreply.github.com>
2026-08-26 22:51:43 -0300

    Redesign and clean CSS
    1 - Melhoria na navbar, seção main e sobre mim. 
    2 - Hover do mouse nas imagens e filter nas imagens.
    3 - Sections provisórias para funcionamento da navbar

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const menuBotao = document.querySelector('.menuBotao');
 const menu = document.querySelector('.navbar ul');
+const links = document.querySelectorAll('.navbar a');
 
 menuBotao.addEventListener('click', function () {
     menu.classList.toggle('menuAberto');
 });
-
-const links = document.querySelectorAll('.navbar a');
 
 links.forEach(function (link) {
     link.addEventListener('click', function () {
